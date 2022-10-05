bb3eb9ae4e80565e4156d74616470a1704709ff6
Alice <andreevaalisa42@gmail.com>
2022-10-05 20:04:44 +0300

    finish skills section

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,24 @@ navLink.forEach(el => el.addEventListener('click', linkAction));
 
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.querySelectorAll('.skills__content');
+const skillsHeader = document.querySelectorAll('.skills__header');
 
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (let i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = 'skills__content skills__closed';
+  }
+  
+  if (itemClass === 'skills__content skills__closed') {
+    this.parentNode.className = 'skills__content skills__open';
+  }
+}
+
+skillsHeader.forEach((el) => {
+  el.addEventListener('click', toggleSkills);
+});
 
 /*==================== QUALIFICATION TABS ====================*/
 
