1a4cb525a94691d07eb0c3b813b9d8a33a19d9ba
Alice <andreevaalisa42@gmail.com>
2022-10-05 16:08:38 +0300

    finish navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,30 @@
-/*==================== MENU SHOW Y HIDDEN ====================*/
+/*==================== MENU SHOW HIDDEN ====================*/
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close'),
+  navLink = document.querySelectorAll('.nav__link');
 
+/*===== SHOW MENU =====*/
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  })
+}
 
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
-
-
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
+/*===== HIDE MENU =====*/
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  })
+}
 
 
 /*==================== REMOVE MENU MOBILE ====================*/
+function linkAction() {
+  navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach(el => el.addEventListener('click', linkAction));
 
 
 /*==================== ACCORDION SKILLS ====================*/
@@ -30,10 +45,10 @@
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 
 
-/*==================== SHOW SCROLL UP ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 
 
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/
